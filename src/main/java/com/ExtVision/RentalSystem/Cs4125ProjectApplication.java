483bc15114777f300c9a4880e152dc1ad5a9136e
Carlos Cruz <Mountaindew7436@gmail.com>
2023-11-28 15:30:13 +0000

    initialised project

---
+++ src/main/java/com/ExtVision/RentalSystem/Cs4125ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ExtVision.RentalSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs4125ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs4125ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ExtVision/RentalSystem/Cs4125ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExtVision.RentalSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs4125ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
